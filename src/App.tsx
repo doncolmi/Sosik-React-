bd10d05ab88e202922d59bdf6dc835bc96734b95
doncolmi <matathresh1740@naver.com>
2020-08-03 17:23:35 +0900

    study 9.Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import { Hello, Bye } from "./component/Hello";
+import Page from "./component/Page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello name="it's me" age={24} />
-        <Bye name="It's you" age={25} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Page name="BAMAM" />
     </div>
   );
 }
--- src/component/Page.tsx
+++ src/component/Page.tsx
@@ -1,4 +1,49 @@
-import React from "react";
+import React, { Component, ChangeEvent } from "react";
+import { Hello } from './Hello';
 import "./Hello.css";
 
-class Page extends React.Component {}
+interface Props {
+    name: string;
+}
+
+interface State {
+    value: string;
+}
+
+class Page extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    state: State = {
+        value: '',
+    }
+
+    handleChange(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({value: e.target.value});
+    }
+
+    componentDidMount() {
+        console.log("did Mount!");
+    }
+
+    componentDidUpdate() {
+        console.log("데이터 바꼇네 ㅋㅋ");
+    }
+
+    render() {
+        const placeHolder: string = "Hello!";
+
+        return (
+            <div>
+                <Hello name="HelloGuy" age={24} />
+                <div>{this.state.value}</div>
+                <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={placeHolder} />
+            </div>
+        );
+    }
+}
+
+export default Page
