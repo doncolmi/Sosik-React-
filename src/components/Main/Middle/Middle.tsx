d31428dfdc378f00c2b087df37f325ecef1523d0
doncolmi <matathresh1740@naver.com>
2020-08-06 22:32:41 +0900

    finish Mobile

--- src/components/Main/Middle/Middle.tsx
+++ src/components/Main/Middle/Middle.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import "../../../css/Main.css";
-import "./Middle.css"
+import "./Middle.css";
 
 import Down from "../Top/Icons/Down";
 import Block from "../../Util/Block/Block";
@@ -18,39 +18,43 @@ const Middle: FC = () => {
     iconClass: "fas fa-thumbs-up",
     exp: `좋아하는 주제들을 팔로우하여,`,
     exp2: `내 취향에 맞는 기사를 볼 수 있습니다.`,
-  }
+  };
 
   const reply: BlockProps = {
     title: "댓글은 보고 싶을 때만!",
     iconClass: "fas fa-hand-sparkles",
     exp: "뉴스의 댓글이 보고 싶지 않을 때는",
-    exp2: "설정을 통하여 댓글을 숨길 수 있습니다."
-  }
+    exp2: "설정을 통하여 댓글을 숨길 수 있습니다.",
+  };
 
   const press: BlockProps = {
     title: "원하는 언론사만!",
     iconClass: "fas fa-thumbs-up",
     exp: `선호하는 언론사들을 팔로우하여,`,
     exp2: `내 취향에 맞는 기사를 볼 수 있습니다.`,
-  }
+  };
 
   return (
     <div className="wrapper section">
       <div className="contents w100h100 flexColumn flexMiddle bgColor">
-        <div className="middleTop flexColumn flexCenter">
+        <div
+          className="middleTop flexColumn flexCenter"
+          data-aos="fade-up"
+          data-aos-delay="100"
+          data-aos-duration="500"
+        >
           <div className="middleTitle">사이트 특징</div>
           <div className="middleExp">소식은 이런 사이트 입니다.</div>
           <hr className="middleHr" />
         </div>
         <div className="middleBottom flexColumn flexCenter">
           <div className="middleBox flexCenter">
-            <Block content={like}/>
-            <Block content={reply}/>
-            <Block content={press}/>
+            <Block content={like} />
+            <Block content={reply} />
+            <Block content={press} />
           </div>
         </div>
-          
-          <Down Message="스크롤해서 사이트 특징 보기" />
+        <Down Message="스크롤해서 사이트 특징 보기" />
       </div>
     </div>
   );
--- src/components/Util/Block/Block.tsx
+++ src/components/Util/Block/Block.tsx
@@ -14,18 +14,29 @@ interface Props {
 
 const Block: FC<Props> = ({ content }: Props) => {
   return (
-    <div className="box shawdow">
+    <div
+      className="box shawdow"
+      data-aos="fade-up"
+      data-aos-delay="100"
+      data-aos-duration="500"
+    >
       <div className="boxTop flexCenter">
-        <div className="boxTitle">{ content.title }</div>
+        <div className="boxTitle">{content.title}</div>
         <hr className="boxHr" />
       </div>
 
       <div className="boxMiddle flexCenter">
-        <span className="boxIcon"><i className={ content.iconClass }></i></span>
+        <span className="boxIcon">
+          <i className={content.iconClass}></i>
+        </span>
       </div>
 
       <div className="boxBottom flexCenter">
-        <div className="boxExp">{ content.exp }<br />{ content.exp2 }</div>
+        <div className="boxExp">
+          {content.exp}
+          <br />
+          {content.exp2}
+        </div>
       </div>
     </div>
   );
