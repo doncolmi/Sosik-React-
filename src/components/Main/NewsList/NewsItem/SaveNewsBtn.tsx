2b9c2ae1ad30333eff68d5e306beaf7168d09d17
doncolmi <matathresh1740@naver.com>
2020-09-21 00:46:12 +0900

    fix SaveNewsBtn

--- src/components/Main/NewsList/NewsItem/SaveNewsBtn.tsx
+++ src/components/Main/NewsList/NewsItem/SaveNewsBtn.tsx
@@ -1,16 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import "../NewsItem.css";
+
+import axios from "axios";
 
 interface Props {
   newsId: string;
 }
 
 const SaveNewsBtn: FC<Props> = ({ newsId }: Props) => {
+  const [isSaved, setIsSaved] = useState<boolean>(false);
 
-    
+  async function getIsSaved(newsId: string) {
+    const { data } = await axios.get(
+      `${process.env["REACT_APP_BACKEND_SERVER"]}/save/${newsId}`
+    );
+    if (typeof data === "boolean") await setIsSaved(data);
+  }
 
+  useEffect(() => {
+    getIsSaved(newsId);
+  }, [newsId, isSaved]);
+
+  if (isSaved) {
+    // todo: 여기 작성하세요.
+  }
   return (
-    <span onClick={() => console.log("하이")}>
+    <span className="saveNewsBtn" onClick={() => console.log("하이")}>
       <i className="fas fa-thumbtack"></i>
     </span>
   );
